9d7a2e53291dbbaa1e08c086437bd1a25c9f2b4b
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2024-09-01 18:34:46 +0700

    fix lỗi

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import registerRequest from '../../request/registerRequest';
 
 @Component({
   selector: 'app-signup',
@@ -12,12 +13,30 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class SignupComponent {
   @Output() toggleMode = new EventEmitter<void>(); // Khai báo EventEmitter
-
+  isRegisterMode = false;
+  formData: registerRequest = {
+    username: '',
+    password: '',
+    repassword: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    address: '',
+    phone: ''
+  }; // Khởi tạo formData với giá trị mặc định
+  onRegisterMode() {
+    this.isRegisterMode = !this.isRegisterMode;
+  }
   onToggleMode() {
     this.toggleMode.emit(); // Gửi sự kiện để chuyển đổi chế độ
   }
   onSubmitRegister(form: NgForm) {
-    // Xử lý đăng ký
-    console.log('Đăng ký:', form.value);
+    const forms = form.value as registerRequest;
+    
+  
+  
+      this.formData = { ...this.formData, ...forms };
+      console.log(this.formData)
+    
   }
 }
---
+++ src/app/request/registerRequest.ts
@@ -0,0 +1,10 @@
+export default interface registerRequest{
+    username:string;
+    password:string;
+    repassword:string;
+    firstname:string;
+    lastname:string;
+    email:string;
+    address:string;
+    phone:string
+}

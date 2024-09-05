9a97d38e33a7ae5282bc0186fcbcbf8877a08e1e
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2024-09-05 18:56:55 +0700

    xong UI header

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import registerRequest from '../../request/registerRequest';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -12,6 +13,7 @@ import registerRequest from '../../request/registerRequest';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  constructor(private http:HttpClient, private router:Router){}
   @Output() toggleMode = new EventEmitter<void>(); // Khai báo EventEmitter
   isRegisterMode = false;
   formData: registerRequest = {
@@ -31,12 +33,22 @@ export class SignupComponent {
     this.toggleMode.emit(); // Gửi sự kiện để chuyển đổi chế độ
   }
   onSubmitRegister(form: NgForm) {
-    const forms = form.value as registerRequest;
-    
-  
-  
+    if(form.valid){
+      const forms = form.value as registerRequest;
       this.formData = { ...this.formData, ...forms };
-      console.log(this.formData)
-    
+      if(!this.isRegisterMode){
+        this.http.post('http://localhost:5000/auth/signup',this.formData)
+        .subscribe({
+          next: (v)=> console.log(v),
+          error: (e) => alert(e.error.message),
+          complete: ()=>{alert('Đăng ký thành công'); this.router.navigate(['/'])},
+        })
+      }
+      }
+     
+    else{
+      console.log('Form is invalid');
+    }
+
   }
 }

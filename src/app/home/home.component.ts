6bec95a58ed9e84d45c111292a62dfe5444fd7f0
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-03-21 17:25:07 +0700

    truyen api loai

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnDestroy } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { Category } from '../../entity/Category';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnDestroy {
-    constructor(private cookieService:CookieService){}
+export class HomeComponent implements OnInit,OnDestroy {
+    category: Category[] = [];
+    constructor(private cookieService:CookieService,private httpClient: HttpClient){}
+  ngOnInit(): void {
+    this.httpClient.get('http://localhost:5000/categories').subscribe((data: any) => {
+      this.category = data.data;
+    });
+    console.log(this.category);
+  }
   ngOnDestroy(): void {
   
   }
-
   
 }

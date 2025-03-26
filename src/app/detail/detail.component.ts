b366316820dd08b17855588b8a9a17c2b721ae10
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-03-26 17:55:03 +0700

    do du lieu vao detail

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Category } from './../../entity/Category';
+import { Component, OnInit, input } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Book } from '../../entity/Book';
 import { CategoryService } from '../../service/CategoryService';
@@ -13,14 +14,25 @@ import { NgFor } from '@angular/common';
 })
 export class DetailComponent implements OnInit {
   id =''
-  books:Book[] =[]; 
+  books:Book[] =[];
+  categorys: Category[] = [];
+  category: string = '';
   constructor(private route: ActivatedRoute,private categoryService: CategoryService) {
     this.id = String(route.snapshot.paramMap.get('id'));
   }
 
   ngOnInit() {
    this.categoryService.getBooksByCategory(Number(this.id)).subscribe({
-     next: (v) => this.books = v.data!,
+     next: (v) => {
+      this.books = v.data!
+    },
   })
+  this.categoryService.getCategories().subscribe({
+    next: (v) => {
+     this.categorys = v.data!
+   }})
 }
+  getNameCategory(){
+    return this.categorys.find((v) => v.categoryId == Number(this.id))?.categoryName;
+  }
 }

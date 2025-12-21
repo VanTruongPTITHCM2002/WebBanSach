f5fbf10a7c1d72f179c38d4bbbe3041c45c4b72b
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-12-21 17:15:13 +0700

    feat(category):refactor-code

--- src/app/admin/category/category.component.ts
+++ src/app/admin/category/category.component.ts
@@ -12,94 +12,119 @@ import Swal from 'sweetalert2';
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrl: './category.component.css',
 })
-export class AdminCategoryComponent implements OnInit{
-    categories: Category[] = [];
-    page: number = 1;
-    getCategories: Subscription;
-    addCategory: Subscription;
-    updateCategory: Subscription;
-    deleteCategory: Subscription;
-    isUpdate: boolean = false;
-    isLastPage = false;
-    currentPage = 1;
-    pageSize = 5;
-    categoryName: string = '';
-    updateCategoryName: string = '';
-    categoryId: number = 0;
-    @ViewChild('closeBtn',{static: false}) closeBtn!: ElementRef<HTMLButtonElement>;
-    
+export class AdminCategoryComponent implements OnInit {
+  categories: Category[] = [];
+  page: number = 1;
+  getCategories: Subscription;
+  addCategory: Subscription;
+  updateCategory: Subscription;
+  deleteCategory: Subscription;
+  txtSearch = null;
+  isUpdate: boolean = false;
+  isLastPage = false;
+  currentPage = 1;
+  pageSize = 5;
+  categoryName: string = '';
+  updateCategoryName: string = '';
+  totalPages: number = 1;
+  loading = false;
+  categoryId: number = 0;
+  @ViewChild('closeBtn', { static: false })
+  closeBtn!: ElementRef<HTMLButtonElement>;
+  @ViewChild('closeBtnUpdate', { static: false })
+  closeBtnUpdate!: ElementRef<HTMLButtonElement>;
 
-    constructor(private categoryService: CategoryService){
-      this.getCategories = new Subscription();
-      this.addCategory = new Subscription();
-      this.updateCategory = new Subscription();
-      this.deleteCategory = new Subscription();
-    }
+  constructor(private categoryService: CategoryService) {
+    this.getCategories = new Subscription();
+    this.addCategory = new Subscription();
+    this.updateCategory = new Subscription();
+    this.deleteCategory = new Subscription();
+  }
 
-    ngOnInit(): void {
-      this.loadCategories(this.currentPage);
-    }
+  ngOnInit(): void {
+    this.loadCategories(this.currentPage);
+  }
 
-
-    loadCategories(page: number){
-      this.getCategories.unsubscribe();
-      this.getCategories = this.categoryService.getCategories(page, this.pageSize).subscribe((data) => {
-          this.categories = data.data || [];
-          this.currentPage = page;
-          this.isLastPage = this.categories.length < this.pageSize;
+  loadCategories(page: number) {
+    this.getCategories.unsubscribe();
+    this.getCategories = this.categoryService
+      .getCategories(page, this.pageSize, this.txtSearch!)
+      .subscribe((data) => {
+        this.categories = data.data.content || [];
+        this.currentPage = page!;
+        this.totalPages = data.data.totalPages;
       });
-    }
+  }
 
-    setPage(page: number){
-      if (page < 1) return; // Không cho trang nhỏ hơn 1
-      if (page === this.currentPage) return; // Không reload trang hiện tại
-      if (this.isLastPage && page > this.currentPage) return; // Nếu là trang cuối rồi, không sang trang tiếp theo
-        this.loadCategories(page);
-    }
+  setPage(page: number) {
+    if (page < 1) return;
+    if (page > this.totalPages) return;
+    if (page === this.currentPage) return;
+    this.loadCategories(page);
+  }
 
-    insertCategory(){
-        this.addCategory = this.categoryService.addCategory(this.categoryName).subscribe((data) =>{
-          showResponseSuccess(data.message);
-        });
-    }
-
-    selectedCategory(categoryName:string, categoryId: number){
-      this.isUpdate = true;
-      this.updateCategoryName = categoryName;
-      this.categoryId = categoryId;
-    }
-
-    patchCategory(){
-      this.updateCategory = this.categoryService.updateCategory(this.categoryId, this.updateCategoryName).subscribe((data) => {
+  insertCategory() {
+     if (!this.categoryName || !this.categoryName.trim()) {
+       showResponseFailure('Vui lòng nhập tên danh mục');
+       return;
+     }
+    this.addCategory = this.categoryService
+      .addCategory(this.categoryName)
+      .subscribe((data) => {
         showResponseSuccess(data.message);
         this.closeBtn.nativeElement.click();
         this.loadCategories(this.page);
-      })
-    }
+      });
+  }
 
-    removeCategory(categoryId: number){
-        Swal.fire({
-                title: "Bạn có chắc chắn muốn xóa danh mục này?",
-                showDenyButton: true,
-                showCancelButton: false,
-                confirmButtonText: "Có",
-                denyButtonText: `Không`
-            }).then((result) => {
-                if (result.isConfirmed) {
-                  this.deleteCategory = this.categoryService.deleteCategory(categoryId).subscribe({
-                    next: (value) => {
-                      if (value.statusCode !== 200) return showResponseFailure(value.message);
-                      showResponseSuccess(value.message);
-                      this.categories = this.categories.filter(category => category.categoryId !== categoryId)
-                    },
-                    error(err) {
-                      showResponseFailure(err.message);
-  
-                    },
-                  })
-                }
-            });
-    }
+  handleSearch() {
+    this.page = 1;
+    this.loadCategories(this.page);
+  }
+
+  selectedCategory(categoryName: string, categoryId: number) {
+    this.isUpdate = true;
+    this.updateCategoryName = categoryName;
+    this.categoryId = categoryId;
+  }
+
+  patchCategory() {
+    this.updateCategory = this.categoryService
+      .updateCategory(this.categoryId, this.updateCategoryName)
+      .subscribe((data) => {
+        showResponseSuccess(data.message);
+        this.closeBtnUpdate.nativeElement.click();
+        this.loadCategories(this.page);
+      });
+  }
+
+  removeCategory(categoryId: number) {
+    Swal.fire({
+      title: 'Bạn có chắc chắn muốn xóa danh mục này?',
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: 'Có',
+      denyButtonText: `Không`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.deleteCategory = this.categoryService
+          .deleteCategory(categoryId)
+          .subscribe({
+            next: (value) => {
+              if (value.statusCode !== 200)
+                return showResponseFailure(value.message);
+              showResponseSuccess(value.message);
+              this.categories = this.categories.filter(
+                (category) => category.categoryId !== categoryId
+              );
+            },
+            error(err) {
+              showResponseFailure(err.message);
+            },
+          });
+      }
+    });
+  }
 }

4f35f5306976830a69fb4683da1fb93c2c822ea8
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-10 15:03:43 +0700

    feat(home):add-link-image

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,32 +12,52 @@ import { BookService } from '../../service/BookService';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink,NgFor],
+  imports: [RouterLink, NgFor],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit,OnDestroy {
-    category: Category[] = [];
-    book: Book[] = [];
-    getCategroy: Subscription;
-    getBook: Subscription;
-    constructor(private cookieService:CookieService,private categoryService: CategoryService
-        ,private bookService:BookService
-    ){
-      this.getCategroy = new Subscription();
-      this.getBook = new Subscription();
+export class HomeComponent implements OnInit, OnDestroy {
+  category: Category[] = [];
+  book: Book[] = [];
+  categories: [
+    {
+      categoryId: string;
+      name: string;
     }
-  ngOnInit(): void {
-     this.getCategroy = this.categoryService.getCategories().subscribe((data)=>{
-      this.category = data.data ?? [];
-     });
-   
-     this.getBook = this.bookService.getBooksByBuys().subscribe((data)=>{
+  ] = [
+    {
+      categoryId: '',
+      name: '',
+    },
+  ];
+  getCategroy: Subscription;
+  getBook: Subscription;
+  images: string[] = ['https://media.istockphoto.com/id/949118068/vi/anh/s%C3%A1ch-v%E1%BB%9F.jpg?s=2048x2048&w=is&k=20&c=rgFGgZSTrcVF1MziZxwfxuTdApVnDlp-uZSJY8iTbOA=',
+    'https://media.istockphoto.com/id/2060984408/vi/anh/ch%E1%BB%93ng-s%C3%A1ch-%C4%91%E1%BA%A7y-m%C3%A0u-s%E1%BA%AFc-n%E1%BB%81n-t%E1%BA%A3ng-gi%C3%A1o-d%E1%BB%A5c-tr%E1%BB%9F-l%E1%BA%A1i-tr%C6%B0%E1%BB%9Dng-h%E1%BB%8Dc-s%C3%A1ch-b%C3%ACa-'
+    +'c%E1%BB%A9ng-%C4%91%E1%BA%A7y-m%C3%A0u-s%E1%BA%AFc-s%C3%A1ch-tr%C3%AAn.jpg?s=2048x2048&w=is&k=20&c=_sRznS_LsGXJ2R_4p-soL2xJ8sSYkyunL_qzyjWBRTo=',
+    '/your-own-library-landing-page-template_151150-680.jpg'
+  ];
+
+
+  constructor(
+    private categoryService: CategoryService,
+    private bookService: BookService
+  ) {
+    this.getCategroy = new Subscription();
+    this.getBook = new Subscription();
+  }
+  async ngOnInit(): Promise<void> {
+    this.getCategroy = this.categoryService
+      .getCategories()
+      .subscribe((data) => {
+        this.category = data.data.content ?? [];
+      });
+
+    this.getBook = this.bookService.getBooksByBuys().subscribe((data) => {
       this.book = data.data ?? [];
-     });
+    });
+
+    this.categories = await this.categoryService.getCategoriesNotPaginate();
   }
-  ngOnDestroy(): void {
-    
-  }
-  
+  ngOnDestroy(): void {}
 }

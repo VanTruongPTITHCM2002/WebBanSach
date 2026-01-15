97310d80aeadde7db9950b87be0dbb264e5439a3
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-15 17:01:41 +0700

    feat(book):add-filter-and-clear-filter

--- src/app/detail/sub-detail/sub-detail.component.ts
+++ src/app/detail/sub-detail/sub-detail.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common";
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { CategoryService } from "../../../service/CategoryService";
 import { Category } from "../../../entity/Category";
 import { Subscription } from "rxjs";
 import { PublisherService } from "../../../service/PublisherService";
 import { Publisher } from "../../../entity/Publisher";
+
 
 @Component({
   selector: 'sub-detail',
@@ -19,6 +20,53 @@ export class SubDetailComponent implements OnInit {
     publisherId: string;
     name: string;
   }[] = [];
+  @Input() selectedPublisher: number | null = 0;
+  @Output() selectedPublisherChange = new EventEmitter<number | null>();
+  @Output()
+  sortChange = new EventEmitter();
+
+  priceRanges = [
+    { min: 0, max: 150000, label: '0 - 150.000 ₫' },
+    { min: 150000, max: 300000, label: '150.000 - 300.000 ₫' },
+    { min: 300000, max: 500000, label: '300.000 - 500.000 ₫' },
+    { min: 500000, max: null, label: '500.000 ₫ - Trở lên' },
+  ];
+
+  @Input() selectedPrice: {
+    min: number | null;
+    max: number | null;
+    label: string;
+  } | null = {
+    min: null,
+    max: null,
+    label: '',
+  };
+
+  @Output() selectedPriceChange = new EventEmitter<{
+    min: number | null;
+    max: number | null;
+    label: string;
+  } | null>();
+
+  handleSort() {
+    this.sortChange.emit();
+  }
+
+  clearFilter() {
+    this.selectedPublisher = 0;
+    this.selectedPublisherChange.emit(0);
+    this.selectedPrice = {
+      min: null,
+      max: null,
+      label: ''
+    };
+    this.selectedPriceChange.emit( {
+      min: null,
+      max: null,
+      label: ''
+    });
+    this.handleSort();
+  }
 
   async ngOnInit(): Promise<void> {}
 }

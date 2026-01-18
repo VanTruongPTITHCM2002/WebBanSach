02de724d7cfa0abc576acb3786ddc9babe4d3f06
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-18 17:38:46 +0700

    feat(signup):remove-field-dto-register-request

--- src/app/request/registerRequest.ts
+++ src/app/request/registerRequest.ts
@@ -2,9 +2,4 @@ export default interface registerRequest{
     username:string;
     password:string;
     repassword:string;
-    firstname:string;
-    lastname:string;
-    email:string;
-    address:string;
-    phone:string;
 }

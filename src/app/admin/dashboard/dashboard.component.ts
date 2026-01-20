0d6b057798f5ee6a6fb069ddb83d8dd8c96c53e3
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-01-20 13:36:22 +0700

    feat(dashboard);add-api-in-on-init

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,28 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { Router, RouterLink } from "@angular/router";
-import { CookieService } from "ngx-cookie-service";
+import { UserService } from "../../../service/user.service";
+import { InvoiceService } from "../../../service/InvoiceService";
+import { CurrencyPipe } from "@angular/common";
+import { OrderService } from "../../../service/OrderService";
 
 @Component({
     selector:'admin-dashboard',
-    imports: [RouterLink],
+    imports: [RouterLink, CurrencyPipe],
     standalone: true,
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.css',
 })
-export class AdminDashboardComponent{
+export class AdminDashboardComponent implements OnInit{
+  
+    numberOfUsers: number = 0;
+    revenue: number = 0;
+    orders: number = 0;
+    userService = inject(UserService);
+    invoiceService= inject(InvoiceService);
+    orderService = inject(OrderService);
+  
+    ngOnInit(): void {
+        this.userService.getNumberOfUsers().subscribe((data) => {
+            this.numberOfUsers = data.data;
+        });
 
-    // constructor(
-    //     private cookieService: CookieService,
-    //     private router: Router
-    // ){
+        this.invoiceService.getTotalRevenue().subscribe((data) => {
+            this.revenue = data.data;
+        });
 
-    // }
+        this.orderService.getSumOrders().subscribe((data) => {
+            this.orders = data.data;
+        });
+    }
 
-    // logOut(){
-    //     this.cookieService.delete('token','/');
-    //     this.cookieService.delete('username','/');
-    //     this.router.navigate([
-    //         '/login'
-    //     ]);
-    // }
 }

131ee745c247dfa597cf07b5e8e1f75dc035c6b6
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-02-06 16:53:30 +0700

    feat(*):add-global-handle-error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+// import { ErrorInterceptor } from '../service/error-interceptor.service';
+import { GlobalErrorHandler } from './global-error-handler';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()),]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes), 
+     provideClientHydration(), 
+     provideHttpClient(
+      withFetch(),
+      // withInterceptors([ErrorInterceptor])
+    ),
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler,
+    }
+    ]
 };

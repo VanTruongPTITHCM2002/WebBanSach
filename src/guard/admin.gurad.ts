9cc82b4ebf41735f4d91e1c5a0190687977c0ace
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2026-02-14 16:58:13 +0700

    feat(*):update-guard-and-fix-localstorage-is-not-defined

--- src/guard/admin.gurad.ts
+++ src/guard/admin.gurad.ts
@@ -1,16 +1,21 @@
-import { Injectable } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { Inject, inject, Injectable, PLATFORM_ID } from "@angular/core";
 import { CanActivate, Router, UrlTree } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  router = inject(Router);
+  private isBrowser: boolean;
+  constructor(@Inject(PLATFORM_ID) private plaformId: object) {
+    this.isBrowser = isPlatformBrowser(this.plaformId);
+  }
 
   canActivate(): boolean | UrlTree {
-    let username = null;
-    if (typeof window !== undefined){
-       username = localStorage.getItem('username'); 
+    let username: string = '';
+    if (this.isBrowser){
+       username = localStorage.getItem('username') ?? username; 
     }
     return username === 'admin' ? true: this.router.parseUrl('/login'); 
   }
